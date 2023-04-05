232887bde680f1e6c6695847bfcb14cc1904aead
Shameer PC <shameerpc5029@gmail.com>
2023-04-06 02:00:11 +0530

    add bug fix

--- lib/view/home/tabs/catogory_screen.dart
+++ lib/view/home/tabs/catogory_screen.dart
@@ -5,8 +5,6 @@ import 'package:news_app/view/content/content_view.dart';
 
 import 'package:provider/provider.dart';
 
-final controller = ScrollController();
-
 class CategoryScreen extends StatelessWidget {
   final String id;
   const CategoryScreen({super.key, required this.id});
@@ -18,81 +16,76 @@ class CategoryScreen extends StatelessWidget {
       provider.getNews(id);
     });
 
-    return RefreshIndicator(
-      onRefresh: () async {
-        Provider.of<HomeController>(context, listen: false).getNews(id);
-      },
-      child: (provider.newList?.results.isEmpty) ?? false
-          ? const Center(child: Text("No News Results"))
-          : SingleChildScrollView(
-              child: Consumer<HomeController>(
-                builder: (context, value, child) {
-                  return value.isLoading2 == true
-                      ? const CircularProgressIndicator()
-                      : Container(
-                          padding: const EdgeInsets.all(20),
-                          child: ListView.separated(
-                            controller: controller,
-                            shrinkWrap: true,
-                            itemBuilder: (context, index) {
-                              return GestureDetector(
-                                onTap: () {
-                                  Navigator.of(context).push(
-                                    MaterialPageRoute(
-                                      builder: (context) {
-                                        return ContantScreen(index: index);
-                                      },
-                                    ),
-                                  );
-                                },
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Container(
-                                      height: 150,
-                                      decoration: BoxDecoration(
-                                        image: DecorationImage(
-                                          fit: BoxFit.fill,
-                                          image: NetworkImage(
-                                            'https://d2qetcusef3plg.cloudfront.net${value.newList?.results[index].image}?? '
-                                            '',
-                                          ),
-                                        ),
-                                        borderRadius: BorderRadius.circular(20),
-                                      ),
-                                    ),
-                                    KSizedBox.kheight10,
-                                    Text(
-                                      value.newList?.results[index].title ?? '',
-                                      style: const TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18,
-                                      ),
-                                    ),
-                                    KSizedBox.kheight10,
-                                    Text(
-                                      value.newList?.results[index].content ??
+    return (provider.newList?.results.isEmpty) ?? false
+        ? const Center(child: Text("Empty"))
+        : SingleChildScrollView(
+            child: Consumer<HomeController>(
+              builder: (context, value, child) {
+                return value.isLoading2 == true
+                    ? const Center(child: CircularProgressIndicator())
+                    : Container(
+                        padding: const EdgeInsets.all(20),
+                        child: ListView.separated(
+                          physics: const ScrollPhysics(),
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            return GestureDetector(
+                              onTap: () {
+                                Navigator.of(context).push(
+                                  MaterialPageRoute(
+                                    builder: (context) {
+                                      return ContantScreen(index: index);
+                                    },
+                                  ),
+                                );
+                              },
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    height: 150,
+                                    decoration: BoxDecoration(
+                                      image: DecorationImage(
+                                        opacity: 1,
+                                        fit: BoxFit.fill,
+                                        image: NetworkImage(
+                                          'https://d2qetcusef3plg.cloudfront.net${value.newList?.results[index].image}?? '
                                           '',
-                                      style: const TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 12,
-                                        color: Colors.grey,
+                                        ),
                                       ),
-                                      maxLines: 2,
+                                      borderRadius: BorderRadius.circular(20),
                                     ),
-                                    KSizedBox.kheight20,
-                                  ],
-                                ),
-                              );
-                            },
-                            separatorBuilder: (context, index) =>
-                                KSizedBox.kheight10,
-                            itemCount: value.newList?.results.length ?? 0,
-                          ),
-                        );
-                },
-              ),
+                                  ),
+                                  KSizedBox.kheight10,
+                                  Text(
+                                    value.newList?.results[index].title ?? '',
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
+                                    ),
+                                  ),
+                                  KSizedBox.kheight10,
+                                  Text(
+                                    value.newList?.results[index].content ?? '',
+                                    style: const TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12,
+                                      color: Colors.grey,
+                                    ),
+                                    maxLines: 2,
+                                  ),
+                                  KSizedBox.kheight20,
+                                ],
+                              ),
+                            );
+                          },
+                          separatorBuilder: (context, index) =>
+                              KSizedBox.kheight10,
+                          itemCount: value.newList?.results.length ?? 0,
+                        ),
+                      );
+              },
             ),
-    );
+          );
   }
 }
